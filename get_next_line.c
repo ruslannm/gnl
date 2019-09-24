75a0baf1cca671a44c026bd7d0ec16cc9e8bc5a4
rgero <rgero@student.21-school.ru>
2019-09-24 18:12:58 +0300

    111

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rgero <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 16:31:45 by rgero             #+#    #+#             */
-/*   Updated: 2019/09/24 17:16:00 by rgero            ###   ########.fr       */
+/*   Updated: 2019/09/24 18:01:52 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,27 +52,29 @@ void	ft_stop_read(int *size, int buff_size)
 
 char	*ft_get_buff(int fd, int buff_size)
 {
-	char	buff[buff_size + 1];
-	static char	*str;
-	char	*temp;
-	int		size;
-	size_t seek;
-	size_t buff_bytes;
+	char	    buff[buff_size + 1];
+	static char *str;
+	char	    *temp;
+	int		    size;
+	size_t      seek;
+	size_t      buff_bytes;
 
 	size = 0;
 	if (str == NULL)
+	{
 		str = (char*)malloc(sizeof(char) * (size + 1));
+		str[0] = 0;
+	}
 	temp = (char*)malloc(sizeof(char) * (size + 1));
-	str[0] = 0;
 	temp[0] = 0;
-	while ((buff_bytes = read(fd, buff, buff_size)) && str && temp && size >= 0)
+	while ((buff_bytes = read(fd, buff, buff_size)) && str && temp )
 	{
 		size += buff_bytes;
 		ft_strcpy(temp, str);
 		free(str);
 		str = (char*)malloc(sizeof(char) * (size + 1));
 		ft_strcpy(str, temp);
-		ft_strlcat(str, buff, size);
+		ft_strlcat(str, buff, size +1);
 		str[size] = '\0';
 		free(temp);
 		temp = (char*)malloc(sizeof(char) * (size + 1));
@@ -81,13 +83,12 @@ char	*ft_get_buff(int fd, int buff_size)
 		while (seek < buff_bytes && buff[seek] != '\n')
 			seek++;
 		if (buff[seek] == '\n')
-			size = -1;
+			break;
 	}
 	free(temp);
 	temp = (char*)malloc(sizeof(char) * (seek + 1));
 	ft_strncpy(temp, str, seek + 1);
-//	ft_strsub(str,seek + 1, )
-
+	str = ft_strsub(str,seek + 1, size - seek);
 	return (temp);
 }
 
@@ -97,7 +98,11 @@ void	ft_read(int fd)
 
 	str = ft_get_buff(fd, BUFF_SIZE);
 	if (str)
+	{
 		ft_putstr(str);
+		str = ft_get_buff(fd, BUFF_SIZE);
+		ft_putstr(str);
+	}
 	if (fd > 1)
 		close(fd);
 }
