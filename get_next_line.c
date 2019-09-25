4398c20ff64c1f320cc4f7e5512794cf1ff140fd
rgero <rgero@student.21-school.ru>
2019-09-25 19:44:55 +0300

    111

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rgero <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 16:31:45 by rgero             #+#    #+#             */
-/*   Updated: 2019/09/25 17:16:35 by rgero            ###   ########.fr       */
+/*   Updated: 2019/09/25 19:34:45 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,47 +16,15 @@
 #include <string.h>
 #include <libft.h>
 
-# define MAX_INT 2147483647
 
-int	get_next_line(const int fd, char **line)
+
+char	*ft_get_buff(int fd, int *res)
 {
-	size_t buff_bytes;
-	size_t seek;
-	char buff[BUFF_SIZE];
-	static char *tmp_line;
-	char *tmp;
-
-	if (tmp_line == 0)
-	{
-		while ((buff_bytes = read(fd, buff, BUFF_SIZE)))
-		{
-			buff[buff_bytes] = '\0';
-			seek = 0;
-			while (seek < buff_bytes || buff[seek] != '\n')
-				seek++;
-			tmp = (char *) malloc(sizeof(char) * (seek + 1));
-			ft_strncpy(tmp, buff, seek + 1);
-			tmp[seek - 1] = '\n';
-			tmp[seek] = '\0';
-		}
-		*line = tmp;
-	}
-	return (1);
-}
-
-void	ft_stop_read(int *size, int buff_size)
-{
-	if (MAX_INT - buff_size < *size)
-		*size = -1;
-}
-
-char	*ft_get_buff(int fd, int buff_size)
-{
-	char	    buff[buff_size + 1];
+	char	    buff[BUFF_SIZE + 1];
 	static char *tail;
-	char	    *ret;
 	int		    size;
 	size_t      buff_bytes;
+	char         *ret;
 
 	if (tail && tail[0] != 0)
 	{
@@ -65,49 +33,69 @@ char	*ft_get_buff(int fd, int buff_size)
 	}
 	else
 	{
-		ret = (char *) malloc(sizeof(char) * (buff_size + 1));
+		ret = (char *) malloc(sizeof(char) * (BUFF_SIZE + 1));
 		size = 0;
 		ret[0] = 0;
 	}
-	while ((buff_bytes = read(fd, buff, buff_size)))
+	while (*res == 2)
 	{
-		size += buff_bytes;
-		ft_strlcat(ret, buff, size +1);
-		//ret[size] = '\0';
-		free(tail);
-		tail = (char *) malloc(sizeof(char) * (size + 1));
-		if ((tail = ft_memchr(ret, '\n', ft_strlen(ret))))
+		if ((buff_bytes = read(fd, buff, BUFF_SIZE)) >= 0)
 		{
-			ret[tail - ret + 1] = '\0';
-			break;
-		}
-		else if (buff_bytes < buff_size)
-			break; //end file
-		else
-		{
-			tail = ft_strdup(ret);
-			free(ret);
-			ret = (char *) malloc(sizeof(char) * (size_t) (size + buff_size + 1));
-			ret = ft_strcpy(ret, tail);
-			//free(tail);
+			size += buff_bytes;
+			ft_strlcat(ret, buff, size + 1);
+			if ((tail = (char *) ft_memchr(ret, '\n', ft_strlen(ret))))
+			{
+				ret[tail - ret] = '\0';
+				tail = tail + 1;
+				*res = 1;
+			} else if (buff_bytes < BUFF_SIZE)
+				*res = 0;
+			else
+			{
+				tail = ft_strdup(ret);
+				free(ret);
+				ret = (char *) malloc(sizeof(char) * (size_t) (size + BUFF_SIZE + 1));
+				ret = ft_strcpy(ret, tail);
+				free(tail);
+			}
 		}
 	}
 	return (ret);
 }
 
+int	get_next_line(const int fd, char **line)
+{
+	int res;
+
+	if (fd < 0 || fd > 10240 || line == NULL || BUFF_SIZE <= 0)
+		return (-1);
+	res = 2;
+	*line = ft_get_buff(fd, &res);
+	return (res);
+}
+
 void	ft_read(int fd)
 {
 	char	*str;
+	int i;
 
-	str = ft_get_buff(fd, BUFF_SIZE);
+	i = get_next_line(fd, &str);
 	if (str)
 	{
-
 		ft_putstr(str);
-		str = ft_get_buff(fd, BUFF_SIZE);
+		ft_putstr("-");
+		ft_putnbr(i);
+		ft_putstr("-");
+		i = get_next_line(fd, &str);
 		ft_putstr(str);
-		str = ft_get_buff(fd, BUFF_SIZE);
+		ft_putstr("-");
+		ft_putnbr(i);
+		ft_putstr("-");
+		i = get_next_line(fd, &str);
 		ft_putstr(str);
+		ft_putstr("-");
+		ft_putnbr(i);
+		ft_putstr("-");
 
 	}
 	if (fd > 1)
