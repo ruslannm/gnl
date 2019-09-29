3c9c4687c587bbb4f6d0e94c0cb289448f2f17f6
rgero <rgero@student.21-school.ru>
2019-09-29 12:45:00 +0300

    111

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rgero <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 16:31:45 by rgero             #+#    #+#             */
-/*   Updated: 2019/09/29 11:55:23 by rgero            ###   ########.fr       */
+/*   Updated: 2019/09/29 12:38:45 by rgero            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,18 @@ int ft_str_split_end(char **ret, char **tail, int noend)
 	{
 		seek = seekchr - *tail;
 		*ret = ft_strsub(*tail, 0, seek);
-		tmp = ft_strsub(*tail, seek + 1, len - seek);
-        free(*tail);
-		*tail = ft_strdup(tmp);
-        free(tmp);
+		if ((len - seek) == 1)
+		{
+			*tail[0] = '\0';
+			free(*tail);
+		}
+		else
+		{
+			tmp = ft_strsub(*tail, seek + 1, len - seek);
+			free(*tail);
+			*tail = ft_strdup(tmp);
+			free(tmp);
+		}
 		res = (noend == 0 ? 0 : 1);
 	}
 	else if (noend == 0)
@@ -52,7 +60,7 @@ char	*ft_get_buff(int fd, int *res)
 	char         *ret;
 
 	size[0] = 0;
-	if (tail && tail[0] != 0)
+	if (tail && tail[0] != '\0')
 	{
 		if (ft_str_split_end(&ret, &tail, *res))
 			return (ret);
@@ -65,25 +73,18 @@ char	*ft_get_buff(int fd, int *res)
 		{
 			size[0] += buff_bytes;
 			ft_strlcat(tail, buff, size[0] + 1);
-			if (buff_bytes < BUFF_SIZE)
-				*res = 0; //end
-			else
-			{
-				if (ft_memchr (tail, '\n', ft_strlen(tail)))
-					*res = *res - 1;
-				ret = ft_strdup(tail);
-				free(tail);
-				tail = (char *)ft_memalloc(sizeof(char) * (size[0] + BUFF_SIZE + 1));
-				tail = ft_strcpy(tail, ret);
-				free(ret);
-			}
+			if (ft_memchr(tail, '\n', ft_strlen(tail)) || buff_bytes < BUFF_SIZE)
+				*res = *res - 1;
+			ret = ft_strdup(tail);
+			free(tail);
+			tail = (char *) ft_memalloc(sizeof(char) * (size[0] + BUFF_SIZE + 1));
+			tail = ft_strcpy(tail, ret);
+			free(ret);
 		}
 		else
 			*res = 0;
 	}
 	ft_str_split_end(&ret, &tail, *res);
-	//if (*res == 0)
-	//	close(fd);
 	return (ret);
 }
 
