6e320b397171d78803275a417f5cfa607ab1bfc9
rgero <rgero@student.21-school.ru>
2019-10-06 18:05:43 +0300

    111

--- get_next_line.c
+++ get_next_line.c
@@ -14,17 +14,17 @@
 
 static void	ft_del(void *content, size_t len)
 {
-	ft_bzero(content, len);
+	len = 0;
 	free(content);
 }
 
-static int		ft_lst_del(t_list *root, int fd, char **tail)
+static void		ft_lst_del(t_list *root, int fd)
 {
 	t_list			*tmp;
 	t_list			*prev;
 
 	if (!root )
-		return (0);
+		return ;
 	tmp = root;
 	prev = NULL;
 	while (tmp)
@@ -33,14 +33,61 @@ static int		ft_lst_del(t_list *root, int fd, char **tail)
 		{
 			prev->next = tmp->next;
 			ft_lstdelone(&tmp, &ft_del);
-			return (0);
+			return ;
 		}
 		prev = tmp;
 		tmp = tmp->next;
 	}
-	prev->next = ft_lstnew(*tail, ft_strlen(*tail));
-	prev->content_size = fd;
-	return (0);
+}
+
+static char		*ft_lst_pop(t_list *root, int fd)
+{
+	t_list	*tmp;
+	t_list	*prev;
+	char 	*ret;
+
+	if (!root)
+		return (NULL);
+	tmp = root;
+	prev = NULL;
+	while (tmp)
+	{
+		if ((int)tmp->content_size == fd)
+		{
+			ret = (char *)tmp->content;
+			prev->next = tmp->next;
+			ft_lstdelone(&tmp, &ft_del);
+			return (ret);
+		}
+		prev = tmp;
+		tmp = tmp->next;
+	}
+}
+
+static t_list		*ft_lst_push(int fd, char *tail)
+{
+	static t_list	*root;
+	t_list			*tmp;
+	t_list			*prev;
+
+	if (!root && tail && tail[0] != 0)
+	{
+		root = ft_lstnew(tail, ft_strlen(tail));
+		root->content_size = fd;
+	}
+	else
+	{
+		tmp = root;
+		while (tmp->next)
+		{
+			if (tmp->content_size == fd)
+				return (root);
+			tmp = tmp->next;
+		}
+		if (tail && tail[0] != '\0')
+			tmp->next = ft_lstnew(tail, ft_strlen(tail));
+	}
+	return (root);
 }
 
 static t_list		*ft_lst_search(int fd, char **tail)
@@ -49,23 +96,30 @@ static t_list		*ft_lst_search(int fd, char **tail)
 	t_list			*tmp;
 	t_list			*prev;
 
-	if (!root )
+	if (!root && *tail)
 	{
 		root = ft_lstnew(*tail, ft_strlen(*tail));
 		root->content_size = fd;
 		return (root);
 	}
-	tmp = root;
-	while (tmp)
+	else
 	{
-		if ((int)tmp->content_size == fd)
-			return (tmp);
-		prev = tmp;
-		tmp = tmp->next;
+		tmp = root;
+		while (tmp)
+		{
+			if ((int) tmp->content_size == fd)
+				return (tmp);
+			prev = tmp;
+			tmp = tmp->next;
+		}
+		if (*tail)
+		{
+			prev->next = ft_lstnew(*tail, ft_strlen(*tail));
+			prev->content_size = fd;
+			return (prev);
+		}
 	}
-	prev->next = ft_lstnew(*tail, ft_strlen(*tail));
-	prev->content_size = fd;
-	return (prev);
+	return (NULL);
 }
 
 int		ft_str_realloc(char **str, int new_len)
@@ -88,29 +142,27 @@ int		ft_str_realloc(char **str, int new_len)
 	return (res);
 }
 
-int		ft_str_split_end(char **tail, char **line, int get_buff, int fd)
+int		ft_str_split_end(t_list **list, char **line, int get_buff, int fd)
 {
-	int				seek;
-	int				l;
-	char			*chr;
-	int				res;
-	static t_list	*list;
+	int		seek;
+	int		l;
+	char	*chr;
+	char	*tail;
+	int		res;
 
 	res = 0;
-	if (!*tail)
+	if (!(*list = ft_lst_push(fd, *line)))
 		return (0);
-	list = ft_lst_search(fd, &(*tail));
-	l = ft_strlen(*tail);
-	if ((chr = ft_memchr(*tail, '\n', l)) || (l > 0 && get_buff < BUFF_SIZE))
+	tail = ft_lst_pop(*list, fd);
+	l = ft_strlen(tail);
+	if ((chr = ft_strchr(tail, '\n')) || (l > 0 && get_buff < BUFF_SIZE))
 	{
 		res = 1;
-		seek = (chr != NULL ? chr - *tail : l);
-		if (!(*line = ft_strsub(*tail, 0, seek)))
+		seek = (chr != NULL ? chr - tail : l);
+		if (!(*line = ft_strsub(tail, 0, seek)))
 			res = -1;
 		if (*line && (l - seek) > 1)
-			list->content = ft_memmove(list->content, chr + 1, l - seek);
-		else
-			ft_memdel((void **)&(*tail));
+			*list = ft_lst_push(fd, chr + 1);
 	}
 	return (res);
 }
@@ -144,28 +196,30 @@ int		ft_get_buff(int fd, char **buff)
 
 int		get_next_line(const int fd, char **line)
 {
-	char		*buff_ret;
-	static char	*tail;
-	int			ret;
-	char		*tmp;
+	char	*buff_ret;
+	//char	*tail;
+	t_list	*list;
+	int		ret;
+	char	*tmp;
 
 	ret = 0;
 	if (fd < 0 || line == NULL || BUFF_SIZE <= 0 || read(fd, NULL, 0) == -1)
 		return (-1);
-	if ((ret = ft_str_split_end(&tail, &(*line), BUFF_SIZE, fd)))
+	if ((ret = ft_str_split_end(&list, &(*line), BUFF_SIZE, fd)))
 		return (ret);
 	ret = ft_get_buff(fd, &buff_ret);
 	if (buff_ret)
 	{
-		if (!tail)
-			tail = ft_strdup("");
-		tmp = ft_strdup(tail);
-		ft_memdel((void **)&tail);
-		tail = ft_strjoin(tmp, buff_ret);
+		if (!list)
+			tmp = ft_strnew(0);
+		else
+			tmp = ft_strdup(list->content);
+		*line = ft_strjoin(tmp, buff_ret);
 		ft_memdel((void **)&tmp);
 	}
 	if (ret != -1)
-		ret = ft_str_split_end(&tail, &(*line), ret, fd);
+		ret = ft_str_split_end(&list, &(*line), ret, fd);
+	//ft_memdel((void **)&tail);
 	ft_memdel((void **)&buff_ret);
 	return (ret);
 }
